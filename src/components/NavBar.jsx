a93ef310f6c6fd65555201c431204cc9c3a06602
Aidan Dagnall <aidandagnall@gmail.com>
2022-05-27 14:09:32 +0100

    Center headers on mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import '../App.css';
 
 function NavBar(props) {
     return (
-        <div className="flex flex-row justify-end space-x-16 py-8 px-16">
+        <div className="flex flex-row sm:justify-end justify-center space-x-16 py-8 px-16">
             <a href='#projects'>Projects</a>
             <a href='#contact'>Contact</a>
         </div>
