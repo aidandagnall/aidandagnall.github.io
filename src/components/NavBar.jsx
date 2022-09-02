e2f2e10f5f1683b0e03fcd2b6ee9064367b6dd0c
Aidan Dagnall <aidandagnall@gmail.com>
2022-09-02 02:34:27 +0100

    Add Work and Education sections

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,9 @@ import '../App.css';
 
 function NavBar(props) {
     return (
-        <div className="flex flex-row sm:justify-end justify-center space-x-16 py-8 px-16">
+        <div className="flex flex-row sm:justify-end justify-center space-evenly space-x-16 py-8 px-16">
+            <a href='#work'>Work</a>
+            <a href='#education'>Education</a>
             <a href='#projects'>Projects</a>
             <a href='#contact'>Contact</a>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,14 @@ import Hero from '../components/Hero'
 import NavBar from '../components/NavBar';
 import ProjectCard from '../components/ProjectCard';
 import FitnessAppScreen from '../res/fitness_app_screen.png'
+import SurgecrossIcon from '../res/surgecross.jpg'
+import LabMonitorIcon from '../res/app-logo.png'
+import NottinghamIcon from '../res/nottingham_cropped.png'
 import AoC from '../res/aoc.png'
 
 function Home(props) {
     return (
-        <div className='relative bg-bg'>
+        <div className='relative bg-bg overscroll-none'>
             <div className="flex flex-col relative bg-bg">
                 <div className="relative flex flex-col sm:h-screen h-min">
 
@@ -21,19 +24,50 @@ function Home(props) {
                 </div>
 
             <div className='bg-local bg-gradient-to-b from-primary to-[#82ac2c] z-50'>
+                <div id="work" className='flex flex-col pt-16 3xl:w-1/2 xl:w-2/3 lg:w-5/6 lg:mx-auto'>
+                    <p className='text-3xl text-bg mx-auto -mb-4'>Work</p>
+                    <ProjectCard
+                        title="Surgecross Software"
+                        description={["Full-stack software engineer helping to create a cross-platform mobile application and accompanying back-end in a small dedicated team, currently working on ", <a className='text-primary font-semibold' href='https://gardia-app.com'>Gardia</a>, ", a gardening platform to help gardeners keep track of their plants and share their results."]}
+                        img={SurgecrossIcon}
+                    />
+                </div>
+                <div id="education" className='flex flex-col pt-16 3xl:w-1/2 xl:w-2/3 lg:w-5/6 lg:mx-auto'>
+                    <p className='text-3xl text-bg mx-auto -mb-4'>Education</p>
+                    <ProjectCard
+                        title={[<span className='nowrap'>MSci Computer Science</span>, " at the ", <span className='nowrap'>University of Nottingham</span>]}
+                        description={[
+                            <p className='text-xl'>2019-2023</p>,
+                            <p className='my-1'>Currently studying for my Integrated Masters</p>,
+                            <p className='my-1'>Completed my dissertation on the Optimisation of the Infrastructure supporting Hybrid Electric Aircraft</p>,
+                            ]}
+                        img={NottinghamIcon}
+                    />
+                </div>
                 <div id="projects" className='flex flex-col pt-16 3xl:w-1/2 xl:w-2/3 lg:w-5/6 lg:mx-auto'>
                     <p className='text-3xl text-bg mx-auto -mb-4'>Projects</p>
+                    <ProjectCard
+                        title="Lab Monitor"
+                        description="As some rooms in UoN's Computer Science department are often booked out for labs, I wanted a way to plan my day so I could avoid certain rooms at busy times.
+                        To address this, I created a cross platform mobile/web app built in Flutter to display current status of labs in
+                        the Computer Science department at University of Nottingham. Back-end is built in Ktor using
+                        MongoDB for storage. Shows the schedule for each room with an expected business level and allows users
+                        to submit live reports to inform others."
+                        img={LabMonitorIcon}
+                        img_styling='rounded-3xl'
+                    />
                     <ProjectCard
                         title='Android Fitness Tracker'
                         description="A native android app developed in Kotlin to track a user's runs, walks and bike rides.
                         Utilises Google Maps API to show the location and route of each exercise, with statistics for easy analysis and a custom
                         tagging system for categorisation. Built to follow MVVM principles using Rooms for storage."
                         img={FitnessAppScreen}
+                        img_styling='object-scale-down'
                     />
                     <ProjectCard
                         title='eCommerce Site with Packt'
-                        description="With a team of students, we developed a site for Packt's industry partners to easily browse their catalogue and make orders.
-                        Built in Laravel to interact with their existing infrastructure."
+                        description="With a team of other students, we developed a site for Packt's industry partners to easily browse their catalogue and make orders.
+                        Built in Laravel to interact with their existing infrastructure, using JWTs for authentication."
                         img='https://www.packtpub.com/media/logo/stores/1/logo.png'
                         img_styling='rounded-none'
                     />
