9af1826cd614f7213ad4a2154114efc434f026b3
Aidan Dagnall <aidandagnall@gmail.com>
2022-09-02 02:33:51 +0100

    Improve clarity of contact widget and add socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,16 @@
+import Socials from "./Socials"
+
 function Contact(props) {
     return(
-        <div id="contact">
-            <div className="bg-bg w-fit mx-auto mb-12 rounded-full px-12 py-4">
-            <a className='text-primary' href="mailto:contact@aidandagnall.com">Get in touch</a>
+        <div id="contact" className="h-96 flex justify-center items-center">
+
+            <div className="bg-bg w-fit mx-auto mb-12 rounded-full px-12 py-4 text-center">
+                <p className="text-2xl my-2">Want to get in touch?</p>
+                <p className='text-xl'>Contact me via <a className='text-primary' href="mailto:contact@aidandagnall.com">email</a> or on my socials</p>
+
+                <div className="flex flex-row justify-center my-2">
+                    <Socials />
+                </div>
             </div>
         </div>
     )
