326ac832b3ab287330bb3c7403df3a6ac40263c7
Aidan Dagnall <aidandagnall@gmail.com>
2022-10-05 22:16:45 +0100

    Remove dark mode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,25 +2,25 @@ import '../App.css';
 
 function NavBar(props) {
     return (
-    <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 z-50">
+    <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded z-50">
     <div class="flex flex-wrap justify-end items-end mx-auto">
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
       <span class="sr-only">Open main menu</span>
       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
     <div class="hidden absolute top-10 left-10 right-10 w-auto md:block md:w-auto" id="navbar-default">
-      <ul class="flex flex-col p-4 mt-4 mb-4 rounded-lg bg-white md:bg-transparent md:flex-row justify-end md:space-x-8 md:mt-0 font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul class="flex flex-col p-4 mt-4 mb-4 rounded-lg bg-white md:bg-transparent md:flex-row justify-end md:space-x-8 md:mt-0 font-medium md:border-0">
         <li>
-          <a href="#work" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:bg-transparent md:p-0 dark:text-white">Work</a>
+          <a href="#work" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:bg-transparent md:p-0">Work</a>
         </li>
         <li>
-          <a href="#education" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Education</a>
+          <a href="#education" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0">Education</a>
         </li>
         <li>
-          <a href="#projects" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
+          <a href="#projects" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0">Projects</a>
         </li>
         <li>
-          <a href="#contact" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <a href="#contact" class="block py-2 pr-4 pl-3 text-gray-700 rounded md:hover:bg-transparent md:border-0 md:p-0">Contact</a>
         </li>
       </ul>
     </div>
