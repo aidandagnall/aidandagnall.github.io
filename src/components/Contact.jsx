674b3eebcf33b1bbad80e3ad2da8efe263e093b1
Aidan Dagnall <aidandagnall@gmail.com>
2022-11-10 16:23:04 +0000

    Update padding on contact bubble for small displays

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import Socials from "./Socials"
 
 function Contact(props) {
     return(
-        <div id="contact" className="h-96 flex justify-center items-center">
+        <div id="contact" className="h-96 flex justify-center items-center mx-4 lg:mx-0">
 
             <div className="bg-bg w-fit mx-auto mb-12 rounded-full px-12 py-4 text-center">
                 <p className="text-2xl my-2">Want to get in touch?</p>
